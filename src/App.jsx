ff9f49eb0457314efbc8bf1001a7ab6d62b7837d
André Fontenele <andre.hiip@gmail.com>
2023-01-05 16:50:45 -0300

    fix: Correção no diretório do componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Nav from './components/nav/Nav'
 import Header from './components/header/Header'
 import About from './components/about/About'
 import Technologies from './components/technologies/Technologies'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
+import Rodape from './components/rodape/Rodape'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -16,6 +17,8 @@ const App = () => {
       <Technologies />
       <Portfolio />
       <Contact />
+      <Rodape />
+      <Footer />
     </div>
   )
 }
